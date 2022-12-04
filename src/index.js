fecde5f7a77f6214eba31303f27dd2f258825bf3
MainOx480854119 <MainOx480854119@gmail.com>
2022-12-04 12:48:02 +0900

    first commit

---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: Object.values(GatewayIntentBits) });
+
+/**
+ * おみくじを引く関数
+ * @returns {string}
+ */
+function getOmikuji() {
+  const items = ['大吉', '吉', '中吉', 'にゃ〜〜〜ん'];
+  const random = Math.floor(Math.random() * items.length);
+  return items[random];
+}
+
+client
+  .on(Events.ClientReady, () => {
+    console.log(`Logged in as ${client.user.tag}`);
+  })
+  .on(Events.MessageCreate, message => {
+    if (message.author.bot || message.author.system) return;
+    if (!message.inGuild()) return;
+
+    if (message.content.startsWith('！おみくじ')) {
+      message.channel.send(getOmikuji());
+      return;
+    }
+    if (message.mentions.has(client.user.id)) {
+      if (message.content.match(/おみくじ/)) {
+        message.channel.send(getOmikuji());
+        return;
+      }
+
+      message.reply('呼びましたか？');
+    }
+  })
+  .login();
+
+process.on('uncaughtException', console.error);
